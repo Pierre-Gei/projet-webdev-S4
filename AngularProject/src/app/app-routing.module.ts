798bde4786841928046719c88544bc89287bfb39
Pierre-Gei <pierregeiguer@gmail.com>
2023-04-12 09:38:11 +0200

    Update packages

--- AngularProject/src/app/app-routing.module.ts
+++ AngularProject/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcceuilComponent } from './acceuil/acceuil.component';
+import { ProduitsComponent } from './produits/produits.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AcceuilComponent
   },
+  {
+    path: 'produits',
+    component: ProduitsComponent
+  }
 ];
 
 @NgModule({
