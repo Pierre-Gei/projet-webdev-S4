587cc29edbcc53caea117d8e5ad4666fd134868c
Pierre-Gei <pierregeiguer@gmail.com>
2023-04-12 08:44:05 +0200

    Création component produits

--- AngularProject/src/app/app.module.ts
+++ AngularProject/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcceuilComponent } from './acceuil/acceuil.component';
+import { ProduitsComponent } from './produits/produits.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AcceuilComponent
+    AcceuilComponent,
+    ProduitsComponent
   ],
   imports: [
     BrowserModule,
